a8765c68915fec9df1c463b974e667ff37594205
lpercegona <lpercegona@gmail.com>
2026-07-25 09:53:59 -0300

    Change button text from 'Buscar' to 'BUSCAR'

--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -184,7 +184,7 @@ function ExplorePage() {
               <Search className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
               <Input className="pl-9" placeholder="Buscar" value={term} onChange={(e) => setTerm(e.target.value)} />
             </div>
-            <Button type="submit">Buscar</Button>
+            <Button type="submit">BUSCAR</Button>
           </form>
 
           {availableFilters.length > 0 ? (
